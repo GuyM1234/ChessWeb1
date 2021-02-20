e521e49aed9a3dfff91cde2f5b16112f20eedb16
GuyM1234 <63429285+GuyM1234@users.noreply.github.com>
2021-02-20 09:03:55 +0200

    must

--- src/Components/Timer.js
+++ src/Components/Timer.js
@@ -1,84 +1,72 @@
+import React from "react"
 
-import React from 'react';
-import Board from "./Components/Board"
-import { Game } from "./Components/Game";
-import SpotInOptions, { get_opp_color } from "./Components/Methods"
-import Timer from "./Components/Timer"
 
-class App extends React.Component {
-  constructor () {
-    super()
-    this.state = {
-      piece: null,
-      game: new Game(),
-      move_options: [],
-      status: "",
-      winner: ""
-    }
-  }
 
-  handleClick = (piece) => {
-    const row = piece.spot[0]
-    const column = piece.spot[1]
-    this.setState(prevState => {
-      if (this.state.piece == null) {
-        if (this.state.game.turn === piece.color) {
-          const move_options = piece.getAvaliableMoves(prevState.game)
-          if (move_options.length > 0) { 
-            return {piece: piece, move_options: move_options}
-          }
+class Timer extends React.Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            color: props.color,
+            time: 900
         }
-      }
-      else{
-        
-        if (SpotInOptions(piece.spot, this.state.move_options)) {
-          prevState.game.board[prevState.piece.spot[0]][prevState.piece.spot[1]].move([row,column],this.state.game)
-          prevState.game.updateTurn()
+    }
+    componentDidMount(){
+        if (this.props.turn === 'b') {
+            this.startClock()
         }
-        return {piece: null, game: prevState.game, move_options: [], status: this.updateStatus(prevState.game)}
-      }
-    })
-  }
+    }
 
-  updateStatus = () => {
-    if (this.state.game.isCheck()) {
-      if (this.state.game.isCheckMate()) {
-        this.setState({winner: get_opp_color(this.state.game.turn)})
-        return "CHECKMATE!"
-      }
-      else{
-        return "CHECK!"
-      }
+    componentDidUpdate (prevProps) {
+        if (prevProps.turn !== this.props.turn) {
+            if (this.props.turn === this.state.color) {
+                this.startClock()
+            }
+            else{
+                this.stopClock()
+            }
+        }
     }
-    else if (this.state.game.isPat()){
-      return "PAT!"
-    }
-    else{
-      return " "
-    }
-  }
 
-  outOfTime = (color) => {
-    this.setState({winner: get_opp_color(color)})
-  }
-  
-  render () {
-    return(
-      <div className="game">
-        <div className="left">
-          <h1 className="status">{this.state.status}</h1>
-          {/* <h1 className="status">{this.state.winner}</h1> */}
-        </div>
-        
-        <Board board = {this.state.game.board} move_options = {this.state.move_options} handleClick = {this.handleClick}/>
-        <div className="timers">
-          <Timer color = 'b' turn = {this.state.game.turn} outOfTime = {this.outOfTime}/>
-          <Timer color = 'w' turn = {this.state.game.turn} outOfTime = {this.outOfTime}/>
-        </div>
-      </div>
-     
-    )
-  }
+    startClock = () => {
+        this.myInterval = setInterval(() => {
+            this.setState(prevState => {
+                if (prevState.time === 1) {
+                    this.stopClock()
+                    return {time: prevState.time - 1, lost: true}
+                }
+                return {time: prevState.time - 1}
+            })
+        }, 1000)
+    }
+
+    stopClock = () => {
+        clearInterval(this.myInterval)
+    }
+    
+    formatTimer = () =>{
+        let time = ""
+        const minutes = Math.floor(this.state.time / 60)
+        const seconds = this.state.time % 60
+        if (minutes < 10) {
+            time += "0" + minutes + ":"
+        }
+        else {
+            time += minutes + ":"
+        }
+        if (seconds < 10) {
+            time += "0" + seconds
+        }
+        else {
+            time += seconds
+        }
+        return time
+    }
+
+    render () {
+        return (
+            <div className="timer">{this.formatTimer()}</div>
+        )
+    }
 }
 
-export default App;
+export default Timer
